c3eefdb3e20e62faf62289b32c5b2bd85d366649
souhailOUARGUI <80362551+souhailOUARGUI@users.noreply.github.com>
2025-03-16 23:42:39 +0000

    paramétrage d'apparence du texte modifiés

--- app/src/main/java/com/example/tp4/MainActivity.kt
+++ app/src/main/java/com/example/tp4/MainActivity.kt
@@ -10,13 +10,21 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.tp4.ui.theme.TP4Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { Text("Bonjour tout le monde") }
+        setContent { Text(
+            text = "Bonjour tout le monde !",
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp,
+            color = Color.Magenta
+        ) }
     }
 }
 
